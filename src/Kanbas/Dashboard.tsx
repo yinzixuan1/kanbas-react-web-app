28e2277065599c964ad3e6c8d646c4ca0cafe27d
zixuanyin-admin <yin.zixu@northeastern.edu>
2024-09-16 03:54:49 -0700

    fifth commit

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
       <h2 id="wd-dashboard-published">Published Courses (12)</h2> <hr />
       <div id="wd-dashboard-courses">
         <div className="wd-dashboard-course">
-          <img src="/reactjs.webp" alt="An image with reactjs logo" width={200} />
+          <img src="/reactjs.webp" alt="A reactjs logo" width={200} />
           <div>
             <Link className="wd-dashboard-course-link"
               to="/Kanbas/Courses/1234/Home">
