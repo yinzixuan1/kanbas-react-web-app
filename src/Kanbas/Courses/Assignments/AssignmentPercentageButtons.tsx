1959aa5996e71a09de8707b98e0e0506fb0aab44
zixuanyin-admin <yin.zixu@northeastern.edu>
2024-10-03 12:58:09 -0700

    button modified

--- src/Kanbas/Courses/Assignments/AssignmentPercentageButtons.tsx
+++ src/Kanbas/Courses/Assignments/AssignmentPercentageButtons.tsx
@@ -1,20 +1,50 @@
-import { GoPlus } from "react-icons/go";
-import {IoEllipsisVertical} from "react-icons/io5";
+// import { GoPlus } from "react-icons/go";
+// import {IoEllipsisVertical} from "react-icons/io5";
 
+
+// export default function AssignmentPercentageButtons() {
+//   return (
+//     <div className="d-flex align-items-center float-end">
+//       {/* Pill-shaped button */}
+//       <span className="badge-pill percentage-button">
+//         40% of Total
+//       </span>
+
+//       {/* Plus icon */}
+//       <GoPlus className="ms-1 me-2 fs-3" /> {/* Space on left and right of the plus icon */}
+
+//       {/* Ellipsis icon */}
+//       <IoEllipsisVertical className="fs-4" />
+//     </div>
+//   );
+// }
+
+import { GoPlus } from "react-icons/go";
+import { IoEllipsisVertical } from "react-icons/io5";
 
 export default function AssignmentPercentageButtons() {
   return (
     <div className="d-flex align-items-center float-end">
       {/* Pill-shaped button */}
-      <span className="badge-pill percentage-button">
+      <span
+        className="badge-pill percentage-button px-3 py-2 d-flex align-items-center"
+        style={{
+          border: '1px solid #ccc',
+          borderRadius: '20px',
+          backgroundColor: '#f1f1f1',
+        }}
+      >
+        {/* 40% of Total Text */}
         40% of Total
       </span>
 
-      {/* Plus icon */}
-      <GoPlus className="ms-1 me-2 fs-3" /> {/* Space on left and right of the plus icon */}
+        {/* Ellipsis icon */}
+        <IoEllipsisVertical className="fs-4 ms-2" />
 
-      {/* Ellipsis icon */}
-      <IoEllipsisVertical className="fs-4" />
+      {/* Plus icon */}
+      <GoPlus className="ms-2 fs-3" />
     </div>
   );
 }
+
+
