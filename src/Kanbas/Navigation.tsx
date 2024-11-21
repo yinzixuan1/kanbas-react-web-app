5fd402f5ec42bffbe5782e964044cb878f4d9164
zixuanyin-admin <yin.zixu@northeastern.edu>
2024-11-20 17:45:14 -0800

    changed navigation

--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -1,18 +1,42 @@
-import {Link} from "react-router-dom";
-import {useLocation} from "react-router";
-// import {useSelector} from "react-redux";
+import { Link, useLocation } from "react-router-dom";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 
-export default function AccountNavigation() {
-  // const { currentUser } = useSelector((state: any) => state.accountReducer);
+export default function KanbasNavigation() {
   const { pathname } = useLocation();
+  const links = [
+    { label: "Dashboard", path: "/Kanbas/Dashboard", icon: AiOutlineDashboard },
+    { label: "Courses",   path: "/Kanbas/Dashboard", icon: LiaBookSolid },
+    { label: "Calendar",  path: "/Kanbas/Calendar",  icon: IoCalendarOutline },
+    { label: "Inbox",     path: "/Kanbas/Inbox",     icon: FaInbox },
+    { label: "Labs",      path: "/Labs",             icon: LiaCogSolid },
+  ];
+
   return (
-    <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-      <Link  to="/Kanbas/Account/Signin"  className={`list-group-item border border-0
-      ${pathname.includes("Signin") ? "active" : "text-danger"}`}>Signin</Link>
-      <Link  to="/Kanbas/Account/Signup"  className={`list-group-item border border-0
-      ${pathname.includes("Signup") ? "active" : "text-danger"}`}>Signup</Link>
-      <Link  to="/Kanbas/Account/Profile"  className={`list-group-item border border-0
-      ${pathname.includes("Profile") ? "active" : "text-danger"}`}>Profile</Link>
+    <div id="wd-kanbas-navigation" style={{ width: 120 }}
+      className="list-group rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
+      <a
+        id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/"
+        className="list-group-item bg-black border-0 text-center">
+        <img src="/images/NEU.png" width="75px" /></a>
+
+      <Link to="/Kanbas/Account" className={`list-group-item text-center border-0 bg-black
+            ${pathname.includes("Account") ? "bg-white text-danger" : "bg-black text-white"}`}>
+        <FaRegCircleUser className={`fs-1 ${pathname.includes("Account") ? "text-danger" : "text-white"}`} />
+        <br />
+        Account
+      </Link>
+
+      {links.map((link) => (
+        <Link key={link.path} to={link.path} className={`list-group-item bg-black text-center border-0
+              ${pathname.includes(link.label) ? "text-danger bg-white" : "text-white bg-black"}`}>
+          {link.icon({ className: "fs-1 text-danger"})}
+          <br />
+          {link.label}
+        </Link>
+      ))}
     </div>
   );
 }
