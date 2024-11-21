2f2f5e6fb074a81b989b8cc9b12846a1d2307996
zixuanyin-admin <yin.zixu@northeastern.edu>
2024-11-20 17:03:31 -0800

    .

--- src/Labs/Lab5/EnvironmentVariables.tsx
+++ src/Labs/Lab5/EnvironmentVariables.tsx
@@ -1,5 +1,6 @@
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function EnvironmentVariables() {
+  console.log("Environment Variable (REMOTE_SERVER):", process.env.REACT_APP_REMOTE_SERVER);
   return (
     <div id="wd-environment-variables">
       <h3>Environment Variables</h3>
