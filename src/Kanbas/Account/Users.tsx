91690537063b28f6840705e123aef1a42d76a792
zixuanyin-admin <yin.zixu@northeastern.edu>
2024-12-07 05:28:39 -0800

    changed setname

--- src/Kanbas/Account/Users.tsx
+++ src/Kanbas/Account/Users.tsx
@@ -7,7 +7,7 @@ export default function Users() {
   const [users, setUsers] = useState<any[]>([]);
   const { uid } = useParams();
   const [role, setRole] = useState("");
-  const [name, setName] = useState("");
+  const [setName] = useState("");
   const createUser = async () => {
     const user = await client.createUser({
       firstName: "New",
@@ -22,7 +22,7 @@ export default function Users() {
   };
 
   const filterUsersByName = async (name: string) => {
-    setName(name);
+    // setName(name);
     if (name) {
       const users = await client.findUsersByPartialName(name);
       setUsers(users);
